9e42247c3b2b91494e214c5435e27194950a9ea4
obivankanoobi <artium1@gmail.com>
2019-06-20 09:26:11 +0300

    two identical subsets update

--- twoIdenticalSubsets.js
+++ twoIdenticalSubsets.js
@@ -8,62 +8,43 @@
 // you start to look for items which can fit into the pattern from biggest to smallest
 // each iteration you remember the sum and see if the next item can fit there.
 
-const set = [15, 5, 20, 10, 35, 10, 5, 10];
+const set = [15, 5, 20, 10, 35, 15, 10];
 
 function twoIdenticalSubsets(set) {
   const setSum = set.reduce((a, b) => a + b);
   const sortedSet = set.sort((a, b) => b - a);
   const eachSubsetSum = setSum / 2;
   const sub_1 = [];
-  const sub_2 = [];
-  let subSetSum = 0;
 
-  // if the sum cant be divided by 2
-  // simply return because it can't be solved
+  let soFar = 0;
+  let i = 0;
+
   if (setSum % 2 !== 0) {
-    return [];
+    return null;
   }
 
-  for (let i = 0; i < sortedSet.length; i++) {
-    const currentItem = sortedSet[i];
-    const nextItem = sortedSet[i + 1];
-
-    if (i === 0) {
-      // if its first iteration push the first and second element into the first subset array
-      sub_1.push(currentItem, nextItem);
-      // add the sum of the first and the second element in the array
-      subSetSum = currentItem + nextItem;
-      if (subSetSum === eachSubsetSum) {
-        // if the first and the second items are equel to {eachSubsetSum}
-        // then we done and can return both arrays
-        return {
-          sub_1,
-          sub_2: sortedSet.slice(2)
-        };
-      }
-    } else {
-      // checking if the next value and sum smaller or equel to to {eachSubsetSum}
-      if (subSetSum + nextItem <= eachSubsetSum) {
-        sub_1.push(nextItem);
-
-        // adding sum to total
-        subSetSum += nextItem;
-      } else {
-        // if the nextItem and the total sum larger then the {eachSubsetSum} push it to the other array
-        sub_2.push(nextItem);
-      }
-
-      if (subSetSum === eachSubsetSum) {
-        return {
-          sub_1,
-          sub_2
-        };
-      }
+  while (soFar < eachSubsetSum) {
+    if (sortedSet[i] + soFar === eachSubsetSum) {
+      sub_1.unshift(sortedSet[i]);
+      return {
+        sub_1,
+        sub_2: sortedSet.filter(i => !sub_1.includes(i))
+      };
     }
+
+    if (sortedSet[i] + soFar < eachSubsetSum) {
+      soFar += sortedSet[i];
+      soFar;
+      sub_1.unshift(sortedSet[i]);
+    } else {
+      soFar += sortedSet[i];
+    }
+
+    if (i === sortedSet.length - 1) {
+      return null;
+    }
+    i++;
   }
-  // if we went through all of our options and there is no solution
-  // return an empty array
-  return [];
 }
 
 console.log(twoIdenticalSubsets(set));
