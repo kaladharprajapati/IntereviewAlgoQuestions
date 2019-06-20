6b8f9700c18ae1c7075e66171a777b2d056dda66
obivankanoobi <artium1@gmail.com>
2019-06-20 17:00:28 +0300

    more clear solution

--- findTheLargestMultiply.js
+++ findTheLargestMultiply.js
@@ -1,12 +1,20 @@
-const arr = [2, 3, 4, -1, -3, -4]
+const arr = [2, 3, 4, -1, -3, -4];
 
 function findLargestMultiply(arr) {
-  console.time("a")
-  const sortedArr = arr.sort((a, b) => a - b); /*?*/
-  const ans = Math.max(a[0] * sortedArr[1] * sortedArr[sortedArr.length - 1], sortedArr[sortedArr.length - 1] * sortedArr[sortedArr.length - 2] * sortedArr[sortedArr.length - 3]);
-  console.timeEnd("a")
-  return ans
-}
+  const sortedArr = arr.sort((a, b) => a - b);
 
+  // taking the two smallest values product,
+  // and multipy it with the largest product
+  const firstScenario =
+    sortedArr[0] * sortedArr[1] * sortedArr[sortedArr.length - 1];
+
+  // taking the biggest values product
+  const secondScenario =
+    sortedArr[sortedArr.length - 1] *
+    sortedArr[sortedArr.length - 2] *
+    sortedArr[sortedArr.length - 3];
+
+  return Math.max(firstScenario, secondScenario);
+}
 
 console.log(findLargestMultiply(arr));
